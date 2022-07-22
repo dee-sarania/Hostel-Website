1c4fb45ac2c7f1e47c66fe7008014aa64b75d51d
dee-sarania <dhritideepasarania@gmail.com>
2022-07-22 15:37:15 +0530

    alignment

--- pages/contact_us.vue
+++ pages/contact_us.vue
@@ -4,15 +4,15 @@
     <div class="container w-auto">
       <p class="m-20 justify-center text-center capitalize text-4xl text-burgundy font-semibold">- Contact Us -</p>
       <div class="card grid grid-cols-3 border rounded-2xl outline-none m-8">
-        <div class="contact bg-burgundy col-span-2 p-10 justify-items-center text-white grid grid-cols-3 border rounded-l-2xl p-14">
+        <div class="contact bg-burgundy col-span-2 justify-items-center text-white grid grid-cols-3 border rounded-l-2xl p-14">
            
-            <div class="one m-8">
+            <div class="m-8">
                   <img src="/icons/logomain.jpeg" alt="">
             </div>
-            <div class="two col-span-2 m-12">
-                <h6 class="font-semibold mb-2">Jiri Women's Hostel</h6>
-              <h6>Tezpur University,<br>Napaam, Tezpur, Assam <br>PIN - 784028</h6>
-              <p class="flex items-center justify-center md:justify-start hover:underline">
+            <div class="col-span-2 ml-20 mt-12 mb-12 ">
+                <h6 class="text-right font-semibold mb-2">Jiri Women's Hostel</h6>
+              <h6 class="text-right">Tezpur University <br> Napaam, Tezpur, Assam - 784028</h6> &nbsp;
+              <p class="flex items-center justify-end md:justify-end hover:text-red-100">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone"
                 class="w-4 mr-2" role="img" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512">
@@ -22,15 +22,15 @@
                 </svg>
                  +91-1234506789
               </p>
-              <p class="flex items-center justify-center md:justify-start mb-4">
+              <p class="flex items-center justify-end md:justify-end mb-4">
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
-                class="mr-2.5 h-5" role="img" xmlns="http://www.w3.org/2000/svg"
+                class="mr-2 h-4" role="img" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512">
                 <path fill="currentColor"
                   d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z">
                 </path>
               </svg>
-              <a href="mailto:poonam@tezu.ernet.in" class="hover:underline">insertemail@gmail.com</a>
+              <a href="mailto:poonam@tezu.ernet.in" class="hover:underline hover:text-red-100">insertemail@gmail.com</a>
               </p>
               
             </div>
