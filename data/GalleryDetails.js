40fc1e28c66ec203280c7f7da8ff0b58cc384240
Chandrima Paul <smartlee2210@gmail.com>
2022-07-22 12:50:11 +0530

    Gallery Details

---
+++ data/GalleryDetails.js
@@ -0,0 +1,10 @@
+export const photo_credit=[
+    {
+        "name": "Pahari Khanikar",
+        "image":"/images/profile/noimage.jpeg",
+        "designation": "Masters",
+        "department": "Mass Communication and Journalism",
+        "phoneNo": "+91 9876542413",
+        "email": "mcm21018@tezu.ernet.in"
+    },
+]
