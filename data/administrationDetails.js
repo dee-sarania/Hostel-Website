da6b3a2eb55e3db15511a3a655eba4be0e158678
RachnaHarlalka <71887838+RachnaHarlalka@users.noreply.github.com>
2022-07-31 17:35:50 +0530

    datas updated with mails

---
+++ data/administrationDetails.js
@@ -0,0 +1,139 @@
+export const wardens= [
+    {
+      "name": "Dr. Poonam Mishra",
+      "image": "images/profile/warden1.jpeg",
+      "designation": "Senior Warden",
+      "department": "Dept. of Food Engg. & Technology",
+      "phoneNo": "----",
+      "email": "poonam@tezu.ernet.in"
+    },
+    {
+        "name": "Dr. Saona Seth",
+        "image":"images/profile/saonamaam.jpeg",
+        "designation": "Warden",
+        "department": "Dept. of Applied Sciences",
+        "phoneNo": "----",
+        "email": "saona@tezu.ernet.in"
+    },
+    ]
+export const prefects=[
+    {
+        "name": "Preetishree Gogoi",
+        "image":"images/profile/prefect.jpg",
+        "designation": "Prefect",
+        "department": "Dept. of Mathematical Sciences",
+        "phoneNo": "----",
+        "email": "msi18009@tezu.ernet.in"
+    },
+    {
+        "name": "Kaberi Changmai",
+        "image":"images/profile/asstPrefect.jpeg",
+        "designation": "Assistant Prefect",
+        "department": "Dept. of Mathematical Sciences",
+        "phoneNo": "----",
+        "email": "msi18014@tezu.ernet.in"
+    },
+
+    {
+        "name": "Nayanita Saikia",
+        "image":"images/profile/nayanita.jpg",
+        "designation": "Assistant Prefect",
+        "department": "Dept. of Food Engg. and Technology",
+        "phoneNo": "----",
+        "email": "feb19011@tezu.ernet.in"
+    },
+    {
+        "name": "Stanzin Tsangyang",
+        "image":"images/profile/stanzin.jpg",
+        "designation": "Assistant Prefect",
+        "department": "Dept. of Mechanical Engineering",
+        "phoneNo": "----",
+        "email": "meb19040@tezu.ernet.in"
+    },
+    ]
+export const officeStaffs=[
+    {
+        "name": "Monalisha Devi ",
+        "image":"images/profile/incharge.jpeg",
+        "designation": "Incharge",
+        "phoneNo": "+91-8723859217",
+        "email": "----"
+    },
+    {
+        "name": "Anjana Gowala ",
+        "image":"images/profile/caretaker.jpeg",
+        "designation": "Caretaker",
+        "phoneNo": "+91-9365573051",
+        "email": "----"
+    },
+    {
+        "name": "Joona moni Bhuyan ",
+        "image":"images/profile/security1.jpeg",
+        "designation": "Security",
+        "phoneNo": "+91-9365442610",
+        "email": "----"
+    },
+    
+    {
+        "name": "Nilima Ekka ",
+        "image":"images/profile/security2.jpeg",
+        "designation": "Security",
+        "phoneNo": "+91-8822443197",
+        "email": "----"
+    },
+    {
+        "name": "Rima Sultana ",
+        "image":"images/profile/security3.jpeg",
+        "designation": "security",
+        "phoneNo": "+91-8486538006",
+        "email": "----"
+    }
+
+]
+export const messConvenors=[
+
+    {
+        "name": "Nayanita Saikia",
+        "image":"images/profile/nayanita.jpg",
+        "designation": "Mess Convenors",
+        "department": "Dept. of Food Engg. and Technology",
+        "phoneNo": "----",
+        "email": "feb19011@tezu.ernet.in"
+    },
+    {
+        "name": "Stanzin Tsangyang",
+        "image":"images/profile/stanzin.jpg",
+        "designation": "Mess Convenors",
+        "department": "Dept. of Mechanical Engineering",
+        "phoneNo": "----",
+        "email": "meb19040@tezu.ernet.in"
+    },
+    ]
+
+export const webTeams=[
+    {
+        "name": "Rachna Harlalka",
+        "image":"images/profile/rachna.jpeg",
+        "designation":"MCA",
+        "department": "Dept. of Computer Science & Engg.",
+        "phoneNo": "----",
+        "email": "csm21005@tezu.ernet.in"
+    },
+    {
+        "name": "Chandrima Paul",
+        "image":"images/profile/ChandrimaP.jpeg",
+        "designation":"B.Tech",
+        "department": "Dept. of Computer Science & Engg.",
+        "phoneNo": "----",
+        "email": "csb21087@tezu.ernet.in"
+    },
+    {
+        "name": "Dhritideepa Sarania",
+        "image":"images/profile/dhritideepa.jpeg",
+        "designation":"B.Tech",
+        "department": "Dept. of Computer Science & Engg.",
+        "phoneNo": "----",
+        "email": "csb21070@tezu.ernet.in"
+    },
+]
+
---
+++ data/faciltyList.js
@@ -0,0 +1,119 @@
+export const rooms = [
+    {
+        "title":"Boarder's room",
+        "image":"facilityPhoto/boarder.jpg",
+        "description":"There are 90 rooms, each includes two closet and, an open wardrobe. Currently a three-sitter room, it also includes three sets of tables and chairs.Routers are connected along the corridors for wifi connection which provide 24*7 internet access."
+    },
+    {
+        "title":"Common room",
+        "image":"facilityPhoto/common_room.jpeg",
+        "description":"A multi-purpose room for boarders situated at the second floor, it is used for any recreational purposes, group activities and, general meetings."
+    },
+    {
+        "title":"Dorm",
+        "image":"facilityPhoto/dorm.jpg",
+        "description":"Situated at the first and the second floor, it is spacious and has a capacity of about 14 heads. This dorm is very useful when some guest arrives or some emergency situation occurs."
+    },
+    
+    {
+        "title":"Pantry room",
+        "image":"facilityPhoto/pantry.jpg",
+        "description":"Available on each floor of the hostel, they are equipped with a water cooler, wash basin and, a counter top. Here, the boarders can use electronic appliances like electric kettle, etc."
+    },
+
+    {
+        "title":"Security Post",
+        "image":"facilityPhoto/security_post.jpg",
+        "description":"Situated near the entrance gate of our hostel, the Security Mahis are always there to lend a helping hand."
+    },
+
+    {
+        "title":"Warden's Office",
+        "image":"facilityPhoto/warden_room.jpg",
+        "description":"Warden's office is arranged such that hostel's administration meetings can be held."
+    }
+]
+
+export const mess = [
+    {
+        "title":"Dining Hall",
+        "image":"facilityPhoto/dininghall.jpeg",
+        "description":"The hostel has a large hall with a seating capacity of around 110 people at a time. The tables and chairs are well maintained and the whole mess area is cleaned after every meal."
+    },
+    {
+        "title":"Kitchen",
+        "image":"facilityPhoto/kitchen.jpg",
+        "description":"Attached alongside the dining hall at the ground floor, this huge and clean kitchen hall is provided with all the basic kitchen requirements to cook a meal of 250+ people at a time."
+    },
+    
+    {
+        "title":"Refrigerator",
+        "image":"facilityPhoto/fridge.jpeg",
+        "description":"A refrigerator is provided for boarder's utilization which is placed at the Dining Hall."
+    }
+]
+
+export const wash = [
+    {
+        "title":"Basin area",
+        "image":"facilityPhoto/basin1.jpeg",
+        "description":"Set of three basins along with mirrors which is sanitized on a regular basis, is provided near bathroom area of all the six wings of this hostel."
+    },
+    {
+        "title":"Washing area",
+        "image":"facilityPhoto/washing.jpg",
+        "description":"Provided with three taps, this marble floored area is quite spacious and is used for washing activity by the boarders."
+    },
+
+    {
+        "title":"Bathroom area",
+        "image":"facilityPhoto/bath.jpg",
+        "description":"Each wing has five bathrooms and five toilets which are properly maintained, is hygenic and, cleaned on a regular basis."
+    },
+    
+    {
+        "title":"Washing Macine",
+        "image":"facilityPhoto/washing_machine.jpg",
+        "description":"Placed on the first floor, it is available to boarders for free of cost from 07:00 to 18:00 everyday."
+    },
+
+    {
+        "title":"PwD Washroom",
+        "image":"facilityPhoto/pwd.jpg",
+        "description":"Situated at the ground floor in each wing, these washrooms are specially designed to accomodate people with disabilties."
+    },
+
+    {
+        "title":"Drying Area",
+        "image":"facilityPhoto/drying.jpeg",
+        "description":"Vertical metal beams are attached in front of the basin area and washing area for drying cloth articles."
+    }
+]
+
+export const gen = [
+    {
+        "title":"Water Purifier and cooler",
+        "image":"facilityPhoto/cooler.jpg",
+        "description":"Available at each floor for safe and purified drinking water.Even water coolers are available."
+    },
+    
+
+    {
+        "title":"Rainwater Harvesting",
+        "image":"facilityPhoto/rainwater harvesting.jpg",
+        "description":"A rainwater harvester has been installed.The water collected from this are used for gardening purpose and many other emergency situation."
+    },
+    {
+        "title":"Cycle Stand",
+        "image":"/facilityPhoto/cycle_stand.jpg",
+        "description":"Under Construction....this will provide shelter and shade to boarders' cycles"
+                                            
+    },
+    {
+        
+        "title":"Daily newspaper",
+        "image":"/facilityPhoto/newspaper3.jpg",
+        "description":"Morning newspaper facility is available for students and staff members."
+    }
+
+]
