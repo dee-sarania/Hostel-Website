2f4c79ea261bd035c6178adc4b44dd7eb5b2f333
dee-sarania <dhritideepasarania@gmail.com>
2023-09-16 23:45:43 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-const options = {"workboxURL":"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.js","importScripts":[],"config":{"debug":false},"cacheOptions":{"cacheId":"dhriti-prod","directoryIndex":"/","revision":"hh8XKZoGZY0W"},"clientsClaim":true,"skipWaiting":true,"cleanupOutdatedCaches":true,"offlineAnalytics":false,"preCaching":[{"revision":"hh8XKZoGZY0W","url":"/hostels/jwh/?standalone=true"}],"runtimeCaching":[{"urlPattern":"/hostels/jwh/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/hostels/jwh/","handler":"NetworkFirst","method":"GET","strategyPlugins":[]}],"offlinePage":null,"pagesURLPattern":"/hostels/jwh/","offlineStrategy":"NetworkFirst"}
-=======
 // THIS FILE SHOULD NOT BE VERSION CONTROLLED
->>>>>>> Stashed changes
 
 // https://github.com/NekR/self-destroying-sw
 
